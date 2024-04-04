a564e529e7f43edc1cf6947faadc35ca9361a66a
lianshufeng <251708339@qq.com>
2024-04-04 21:02:23 +0800

    更新默认的模型名

--- Qwen1.5/fastchat.sh
+++ Qwen1.5/fastchat.sh
@@ -1,6 +1,6 @@
 #/!bin/bash
 
 nohup python3 -m fastchat.serve.controller --host 0.0.0.0 &
-nohup python3 -m fastchat.serve.model_worker --model-names "gpt-3.5-turbo,Qwen1.5-0.5B-Chat" --model-path /models --device cpu --host 0.0.0.0 &
+nohup python3 -m fastchat.serve.model_worker --model-names "gpt-3.5-turbo,Qwen1.5" --model-path /models --device cpu --host 0.0.0.0 &
 nohup python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port 8000
 
