18c15d75118ab3df0d043d68d669a9365d5a4122
lianshufeng <251708339@qq.com>
2024-12-03 18:50:03 +0800

    将base64转换为本地图片，兼容视频的格式

--- Qwen2-VL/src/server.py
+++ Qwen2-VL/src/server.py
@@ -1,8 +1,12 @@
-import threading
+import base64
+import os
+import tempfile
 import uuid
 from argparse import ArgumentParser
+from io import BytesIO
 
-from flask import Flask, request, jsonify, send_from_directory
+from PIL import Image
+from flask import Flask, request, jsonify
 from flask_cors import cross_origin
 from qwen_vl_utils import process_vision_info
 from transformers import Qwen2VLForConditionalGeneration, AutoProcessor
@@ -50,16 +54,78 @@ def _get_args():
     return parser.parse_args()
 
 
+def itemToLocalFile(image, files):
+    # 循环 items;
+    if image.startswith("data:image"):
+        data = image.split(";", 1)[1]
+        if data.startswith("base64,"):
+            data = base64.b64decode(data[7:])
+            image_obj = Image.open(BytesIO(data))
+            image_rgb = image_obj.convert('RGB')
+            # 生成一个随机的文件名
+            file = tempfile.gettempdir() + '/' + str(uuid.uuid4()) + ".jpg"
+            # 图片保存到本地
+            image_rgb.save(file)
+            image_obj.close()
+            image_rgb.close()
+            files.append(file)
+            return "file://" + file
+    return None
+
+
+def toLocalFile(content, name):
+    files = []
+    if not name in content:
+        return files
+    image = content[name]
+    if isinstance(image, list):
+        # 循环数组，需要下标
+        for index, item in enumerate(image):
+            ret = itemToLocalFile(item, files)
+            if ret is not None:
+                content[name][index] = ret
+    else:
+        content[name] = itemToLocalFile(image, files)
+    return files
+    # if image.startswith("data:image"):
+    #     data = image.split(";", 1)[1]
+    #     if data.startswith("base64,"):
+    #         data = base64.b64decode(data[7:])
+    #         image_obj = Image.open(BytesIO(data))
+    #         # 生成一个随机的文件名
+    #         file = tempfile.gettempdir() + '/' + str(uuid.uuid4()) + ".jpg"
+    #         # 图片保存到本地
+    #         image_obj.save(file)
+    #         files.append(file)
+    #         image_obj.close()
+    #         # 修改content
+    #         content[name] = "file://" + file
+    # return files
+
+
+# 转换为本地的
+def convertLocalFile(messages):
+    files = []
+    # 循环content
+    for message in messages:
+        for content in message['content']:
+            # 添加集合
+            files += toLocalFile(content, 'image')
+            files += toLocalFile(content, 'video')
+    return files
 
 
 @app.route('/compatible-mode/v1', methods=['POST'])
 @cross_origin()
 def compatibleModeV1():
     global model, processor, args
+    files = []
     try:
         data = request.get_json()
         messages = data.get('messages', [])
-        # print(messages)
+
+        # base64 转 本地文件
+        files += convertLocalFile(messages)
 
         text = processor.apply_chat_template(
             messages, tokenize=False, add_generation_prompt=True
@@ -86,7 +152,7 @@ def compatibleModeV1():
 
         # 定义一个dic
         output_text = {
-            "id": "chatcmpl-"+str(uuid.uuid4()),
+            "id": "chatcmpl-" + str(uuid.uuid4()),
             "choices": [
                 {
                     "finish_reason": "stop",
@@ -107,6 +173,13 @@ def compatibleModeV1():
     except Exception as e:
         print(e)
         return jsonify({"error": str(e)}), 500
+    finally:
+        # 删除 files 的所有文件
+        for file in files:
+            try:
+                os.remove(file)
+            except Exception as e:
+                print(e)
 
 
 if __name__ == '__main__':
