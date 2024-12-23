cb28bb6bf72396c857de80835d3b949ce98768a5
lianshufeng <251708339@qq.com>
2024-12-23 23:30:44 +0800

    update

--- DeepFace/src/modules/core/routes.py
+++ DeepFace/src/modules/core/routes.py
@@ -1,26 +1,25 @@
 # built-in dependencies
-import time
-from typing import Union
+from typing import Any
 
 import cv2
 import numpy as np
 # project dependencies
-from deepface import DeepFace
 from deepface.api.src.modules.core import service
+from deepface.commons.image_utils import load_image
 from deepface.commons.logger import Logger
 # from deepface.commons import image_utils
 # 3rd party dependencies
 from flask import Blueprint, request
-from werkzeug.datastructures import FileStorage
 
 logger = Logger()
 
 blueprint2 = Blueprint("routes/v2", __name__)
 
 # 加载 OpenCV 的人脸检测分类器
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-default_face_size = 128
+# 设置最大尺寸
+default_image_max_size = 640
 
 
 # @blueprint2.route("/")
@@ -28,8 +27,7 @@ default_face_size = 128
 #     return f"<h1>Welcome to DeepFace API v{DeepFace.__version__}!</h1>"
 
 
-def extract_image_from_request(img_key: str, faceSize: int = default_face_size, keepSourceImage: bool = False) -> Union[
-    str, np.ndarray]:
+def extract_image_from_request(img_key: str, image_max_size: int) -> tuple[Any, float]:
     """
     Extracts an image from the request either from json or a multipart/form-data file.
 
@@ -54,12 +52,14 @@ def extract_image_from_request(img_key: str, faceSize: int = default_face_size, 
         if file.filename == "":
             raise ValueError(f"No file uploaded for '{img_key}'")
 
-        img = imdecode(file, faceSize, keepSourceImage)
+        # 读取到内存
+        image_array = np.asarray(bytearray(file.stream.read()), dtype=np.uint8)
+        img, scale = imageCode(cv2.imdecode(image_array, cv2.IMREAD_COLOR), image_max_size)
         # 判断非None
         if img is None:
             raise ValueError(f"Images doesn't have face in {img_key}")
 
-        return img
+        return img, scale
     # Check if the request is coming as base64, file path or url from json or form data
     elif request.is_json or request.form:
         input_args = (request.is_json and request.get_json()) or request.form.to_dict()
@@ -68,12 +68,12 @@ def extract_image_from_request(img_key: str, faceSize: int = default_face_size, 
             raise ValueError("empty input set passed")
 
         # this can be base64 encoded image, and image path or url
-        img = input_args.get(img_key)
+        buf, _ = load_image(input_args.get(img_key))
+        img, scale = imageCode(buf, image_max_size)
+        if img is None:
+            raise ValueError(f"Images doesn't have face in {img_key}")
 
-        if not img:
-            raise ValueError(f"'{img_key}' not found in either json or form data request")
-
-        return img
+        return img, scale
 
     # If neither JSON nor file input is present
     raise ValueError(f"'{img_key}' not found in request in either json or form data")
@@ -83,18 +83,22 @@ def extract_image_from_request(img_key: str, faceSize: int = default_face_size, 
 def represent():
     input_args = (request.is_json and request.get_json()) or request.form.to_dict()
 
-    face_size = int(input_args.get("face_size", default_face_size))
-    keep_source_image = bool(input_args.get("keep_source_image", False))
+    # face_size = int(input_args.get("face_size", default_face_size))
+    # keep_source_image = bool(input_args.get("keep_source_image", False))
+
+    image_max_size = int(input_args.get("image_max_size", default_image_max_size))
+    detector_backend = input_args.get("detector_backend", "yunet")  # opencv
+    model_name = input_args.get("model_name", "VGG-Face")
 
     try:
-        img = extract_image_from_request("img", face_size, keep_source_image)
+        img, scale = extract_image_from_request("img", image_max_size)
     except Exception as err:
         return {"exception": str(err)}, 400
 
     obj = service.represent(
         img_path=img,
-        model_name=input_args.get("model_name", "VGG-Face"),
-        detector_backend=input_args.get("detector_backend", "opencv"),
+        model_name=model_name,
+        detector_backend=detector_backend,
         enforce_detection=bool(input_args.get("enforce_detection", True)),
         align=bool(input_args.get("align", True)),
         anti_spoofing=bool(input_args.get("anti_spoofing", False)),
@@ -102,6 +106,10 @@ def represent():
     )
 
     logger.debug(obj)
+
+    obj['scale'] = scale
+    obj['detector_backend'] = detector_backend
+    obj['model_name'] = model_name
 
     return obj
 
@@ -180,13 +188,34 @@ def represent():
 #     return imdecode(image_array)
 
 
-def imdecode(file: FileStorage, faceSize: int, keepSourceImage: bool = False):
-    image_array = np.asarray(bytearray(file.stream.read()), dtype=np.uint8)
+def imageCode(image: np.ndarray, image_max_size: int):
+    # image_array = np.asarray(bytearray(file.stream.read()), dtype=np.uint8)
     # 解码图片
-    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-    # 保留原始图片不需要提取人脸
-    if keepSourceImage:
-        return image
+    # image = cv2.imdecode(buf, cv2.IMREAD_COLOR)
+
+    # 测试代码，全部转换为640
+    # 获取图像的原始宽度和高度
+    height, width = image.shape[:2]
+
+    # 如果小于实际分辨率则直接返回
+    if (height <= image_max_size and width <= image_max_size):
+        return image, 1
+
+    # 判断宽度和高度，确保最大尺寸为640
+    if width > height:  # 如果宽度较大
+        scale = image_max_size / width
+    else:  # 如果高度较大
+        scale = image_max_size / height
+
+    # 根据缩放比例计算新的宽度和高度
+    new_width = int(width * scale)
+    new_height = int(height * scale)
+
+    image = cv2.resize(image, (new_width, new_height))
+    # cv2.imshow("Face Detection", image)
+    # cv2.waitKey(0)
+
+    return image, scale
 
     # # 将图片转换为灰度图（用于人脸检测）
     # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -196,41 +225,41 @@ def imdecode(file: FileStorage, faceSize: int, keepSourceImage: bool = False):
     # 将图片转换为灰度图（用于人脸检测）
     # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     # 使用 Haar 分类器检测人脸
-    startTime = time.time()
-    faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=2, minSize=(20, 20))
-    print(time.time() - startTime)
-
-    # faces = face_cascade.detectMultiScale(image, scaleFactor=1.05, minNeighbors=5, minSize=(32, 32))
-
-    if len(faces) > 0:
-        # 存储最大的面部
-        largest_face = None
-        max_area = 0
-
-        # 遍历所有检测到的人脸
-        for (x, y, w, h) in faces:
-            # 计算每张人脸的面积
-            area = w * h
-
-            # 选择面积最大的（即离摄像头最近的）人脸
-            if area > max_area:
-                max_area = area
-                largest_face = (x, y, w, h)
-
-        # 如果找到了最大的（最靠近的）人脸
-        if largest_face:
-            (x, y, w, h) = largest_face
-            # 裁剪出最大的人脸区域
-            face_region = image[y:y + h, x:x + w]
-
-            # 压缩分辨率（可选）
-            face_region = cv2.resize(face_region, (faceSize, faceSize))
-
-            # 显示人脸
-            # cv2.imshow("Face Detection", face_region)
-            # cv2.waitKey(0)
-
-            return face_region
-
-    # 如果没有检测到任何人脸，返回 None
-    return None
+    # startTime = time.time()
+    # faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=2, minSize=(20, 20))
+    # print(time.time() - startTime)
+    #
+    # # faces = face_cascade.detectMultiScale(image, scaleFactor=1.05, minNeighbors=5, minSize=(32, 32))
+    #
+    # if len(faces) > 0:
+    #     # 存储最大的面部
+    #     largest_face = None
+    #     max_area = 0
+    #
+    #     # 遍历所有检测到的人脸
+    #     for (x, y, w, h) in faces:
+    #         # 计算每张人脸的面积
+    #         area = w * h
+    #
+    #         # 选择面积最大的（即离摄像头最近的）人脸
+    #         if area > max_area:
+    #             max_area = area
+    #             largest_face = (x, y, w, h)
+    #
+    #     # 如果找到了最大的（最靠近的）人脸
+    #     if largest_face:
+    #         (x, y, w, h) = largest_face
+    #         # 裁剪出最大的人脸区域
+    #         face_region = image[y:y + h, x:x + w]
+    #
+    #         # 压缩分辨率（可选）
+    #         face_region = cv2.resize(face_region, (faceSize, faceSize))
+    #
+    #         # 显示人脸
+    #         # cv2.imshow("Face Detection", face_region)
+    #         # cv2.waitKey(0)
+    #
+    #         return face_region
+    #
+    # # 如果没有检测到任何人脸，返回 None
+    # return None
