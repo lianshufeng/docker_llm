89781191313154422d93f2c66627d7a2d42448c5
lianshufeng <251708339@qq.com>
2025-01-03 21:58:11 +0800

    update

---
+++ yolo/demo/video.py
@@ -0,0 +1,38 @@
+import cv2
+
+from ultralytics import YOLO
+
+# Load the YOLO11 model
+model = YOLO("../yolo11n-pose.pt")
+
+# cpu
+model.to('cpu')
+
+# Open the video file
+cap = cv2.VideoCapture(1)
+
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLO11 tracking on the frame, persisting tracks between frames
+        results = model.track(frame, persist=True)
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLO11 Tracking", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
